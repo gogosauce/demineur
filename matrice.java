323ee7813affa4be18ce495517d6051b090643ba
gogosauce <hgo.roger@wanadoo.fr>
2016-12-08 11:24:39 +0100

    Update and rename README.md to matrice.java

---
+++ matrice.java
@@ -0,0 +1,61 @@
+# demineur
+
+public class Matrice {
+	// creation de la matrice 
+	private int[][] mat=new int[8][8];
+	private int nbMines=10; // nb de mines à placer: par defaut 10
+	
+	public Matrice(){
+		// par defaut la matrice est de dimension 8x8
+		// initialisation de la matrice à zero
+		for (int i=0; i<8; i++){
+			for (int j=0; j<8; j++){
+				mat[i][j]=0;
+			}
+		}
+	}
+	public void placeMines(){
+		int x, y; //position de la mine
+		int i=0; //compteur du nb de mines
+		while (i<10){
+			//pour les 10 mines
+			x=(int)Math.round(Math.random()*7);
+			y=(int)Math.round(Math.random()*7);
+			if (mat[x][y]==0){
+				mat[x][y]=9; //9 identifiant de la mine 
+				i++;
+			}
+		}
+		
+	}
+	// fonction qui incrémente les valeurs autour d'une case [x][y] 
+	public void incremCadre ( int m, int n, int x, int y){
+		for (int i=x-1; i<=x+1; i++){
+			for (int j=y-1; j<=y+1; j++){
+				if (i!=x || j!=y){
+					if (i!=-1 && j!=-1 && i!=n+1 && j!=m+1){
+						if (mat[i][j]!=9){
+							mat[i][j]+=1;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	//faire une fonction qui parcourt la matrice pour trouver les 9 et utiliser incremCadre
+
+	public void afficheMat(){
+		for (int i=0; i<8; i++){
+			for (int j=0; j<8; j++){
+				System.out.print(mat[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	
+}
+
+
