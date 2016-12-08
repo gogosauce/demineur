060aebe0a528f3609e1addd3bebf181f98f6def1
Szarlotka <leon.charlott@gmail.com>
2016-12-08 18:20:06 +0100

    update
    tp travail 08/12/2016

--- case.java
+++ case.java
@@ -1,1 +1,27 @@
 
+
+public class Case {
+	private boolean etat; //true decouvert, false couvert
+	private int val; // le chiffre contenu dans la case
+	
+	public Case(){
+		etat=false;
+		val=0;
+	}
+	
+	public int getVal(){
+		return val;
+	}
+	
+	public void setVal(int x){
+		val=x;
+	}
+	
+	public boolean getEtat(){
+		return etat;
+	}
+	
+	public void setEtat(boolean etat){
+		this.etat=etat;
+	}
+}
