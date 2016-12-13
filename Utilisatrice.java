e6e5f510b9137c3e98e9273c17a64f1a703aa38f
Szarlotka <leon.charlott@gmail.com>
2016-12-13 11:12:14 +0100

    Update Utilisatrice.java
    ajout de l'utilsation de la fonction gagner 
    et utilisation du constructeur custom

--- Utilisatrice.java
+++ Utilisatrice.java
@@ -8,14 +8,14 @@ public class Utilisatrice {
 	public static void main(String[] args) {
 		int value=0;
 		// partie permettant de créer la grille ("nouveau jeu")
-		Matrice mat1= new Matrice();
+		Matrice mat1= new Matrice(4,4,9);
 		mat1.placeMines();
 		mat1.incremCadre();
 		mat1.afficheMat(); //affichage version développeur
 		mat1.afficheMatJoueur();  //affichage version joueur 
 		
 		Scanner sc = new Scanner(System.in);
-		while (value!=9){ //tant que l'on ne tombe pas sur une mine on continue de jouer
+		while (value!=9 && mat1.gagner()==false){ //tant que l'on ne tombe pas sur une mine on continue de jouer
 			System.out.println("saisir les coordonnées x y");
 			int x=sc.nextInt();
 			int y=sc.nextInt();
@@ -23,14 +23,19 @@ public class Utilisatrice {
 			if (value!=9){ // si pas de bombe
 				mat1.getMat(x, y).setEtat(true); // on decouvre la case pour devoiler le chiffre
 				if (value==0){ // si on trouve un zero 
-					mat1.recursive(x, y); // on decouvre les cases autour
+					mat1.decouvrezero_rec(x, y); // on decouvre les cases autour
 				}
 			}else{
 				System.out.println("vous avez perdu!");
 				mat1.getMat(x, y).setEtat(true);
+				
 			}
 			mat1.afficheMatJoueur(); //affiche la matrice à jour
 		}
+		if (mat1.gagner()==true){
+			System.out.println("vous avez gagné");
+		}
+		
 		
 	}
 
