69b23e9e6c1a10bd8ea02952c1bcc189d737d6ee
gogosauce <hgo.roger@wanadoo.fr>
2017-01-05 17:10:35 +0100

    Update Utilisatrice.java

--- Utilisatrice.java
+++ Utilisatrice.java
@@ -2,42 +2,54 @@ import java.util.Scanner;
 
 public class Utilisatrice {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		int value=0;
-		// partie permettant de créer la grille ("nouveau jeu")
-		Matrice mat1= new Matrice(4,4,9);
-		mat1.placeMines();
-		mat1.incremCadre();
-		mat1.afficheMat(); //affichage version développeur
-		mat1.afficheMatJoueur();  //affichage version joueur 
-		
-		Scanner sc = new Scanner(System.in);
-		while (value!=9 && mat1.gagner()==false){ //tant que l'on ne tombe pas sur une mine on continue de jouer
-			System.out.println("saisir les coordonnées x y");
-			int x=sc.nextInt();
-			int y=sc.nextInt();
-			value=mat1.getMat(x, y).getVal(); // on enregistre la valeur de la case ds la variable value
-			if (value!=9){ // si pas de bombe
-				mat1.getMat(x, y).setEtat(true); // on decouvre la case pour devoiler le chiffre
-				if (value==0){ // si on trouve un zero 
-					mat1.decouvrezero_rec(x, y); // on decouvre les cases autour
-				}
-			}else{
-				System.out.println("vous avez perdu!");
-				mat1.getMat(x, y).setEtat(true);
-				
-			}
-			mat1.afficheMatJoueur(); //affiche la matrice à jour
-		}
-		if (mat1.gagner()==true){
-			System.out.println("vous avez gagné");
-		}
-		
-		
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	Case.Type type = Case.Type.Empty;
+	int value = 0;
+	boolean isGameEnded = false;
+	
+	// partie permettant de créer la grille ("nouveau jeu")
+	Matrice mat1 = new Matrice(30,16,99);
+	mat1.placeMines();
+	mat1.incremCadre();
+	mat1.afficheMat(); //affichage version dÃ©veloppeur
+	mat1.afficheMatJoueur();  //affichage version joueur 
+
+	Scanner sc = new Scanner(System.in);
+	while (!isGameEnded){
+	    System.out.println("saisir les coordonnÃ©es x y");
+	    int x = sc.nextInt();
+	    int y = sc.nextInt();
+	    Case myCase  = mat1.getMat(x, y);
+
+	    discoverCase(mat1, myCase, x, y);
+	    isGameEnded = checkEndGame(mat1, myCase);
+	    
+	    mat1.afficheMatJoueur(); //affiche la matrice à jour
 	}
+	displayEnd(mat1.gagner());
+    }
 
+    private static void discoverCase(Matrice mat1, Case myCase, int x, int y) {
+	if (myCase.getType() == Case.Type.Empty) {
+	    if (myCase.getVal() == 0){
+		mat1.decouvrezero_rec(x, y); // on decouvre les cases autour
+	    }
+	}
+        myCase.setEtat(Case.State.Discovered);
+    }
+    
+    private static boolean checkEndGame(Matrice mat1, Case myCase) {
+	return (mat1.gagner() == true || myCase.getType() == Case.Type.Mine);
+    }
 
+    private static void displayEnd(boolean isVictory) {
+	if (isVictory){
+	    System.out.println("Vous avez gagné :)");
+	} else {
+	    System.out.println("Vous avez perdu :(");
+	}
+    }
 }
